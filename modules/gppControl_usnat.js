33bcc913c2abda917160d1260b66479005df97c0
Demetrio Girardi <dgirardi@prebid.org>
2026-04-23 00:12:04 -0700

    GPP control modules: add option to restrict more activities (#14762)

--- modules/gppControl_usnat.js
+++ modules/gppControl_usnat.js
@@ -1,12 +1,12 @@
 import { config } from '../src/config.js';
-import { setupRules } from '../libraries/mspa/activityControls.js';
+import { getRules, setupRules } from '../libraries/mspa/activityControls.js';
 import { US_NAT_SID } from '../libraries/mspa/usSections.js';
 
-let setupDone = false;
+let unregister = null;
 
 config.getConfig('consentManagement', (cfg) => {
-  if (cfg?.consentManagement?.gpp != null && !setupDone) {
-    setupRules('usnat', [US_NAT_SID]);
-    setupDone = true;
+  if (cfg?.consentManagement?.gpp != null) {
+    if (unregister != null) unregister();
+    unregister = setupRules('usnat', [US_NAT_SID], getRules(cfg.consentManagement.gpp.mspa?.restrictActivities));
   }
 })
