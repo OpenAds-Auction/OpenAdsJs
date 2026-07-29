prebid/Prebid.js

Various modules: consolidate page view ID logic (#14051)
* Page view ID.

* Page view ID.

* Removed console.log.

* Removed unused import.

* Improved example.

* Fixed some tests.

libraries/pbsExtensions/processors/pageViewIds.js
@@ -1,0 +1,9 @@
+import {deepSetValue} from '../../../src/utils.js';
+
+export function setRequestExtPrebidPageViewIds(ortbRequest, bidderRequest) {
+  deepSetValue(
+    ortbRequest,
+    `ext.prebid.page_view_ids.${bidderRequest.bidderCode}`,
+    bidderRequest.pageViewId
+  );
+}

OpenAds-Auction/OpenAdsJs

libraries/pbsExtensions/processors/pageViewIds.js
@@ -1,0 +1,9 @@
+import {deepSetValue} from '../../../src/utils.js';
+
+export function setRequestExtPrebidPageViewIds(ortbRequest, bidderRequest) {
+  deepSetValue(
+    ortbRequest,
+    `ext.openads.page_view_ids.${bidderRequest.bidderCode}`,
+    bidderRequest.pageViewId
+  );
+}