f94b56a8b17a89ada8728ba0c272e40d1f890c5f
Khang Vu <khang.vu@thetradedesk.com>
2026-08-12 10:22:11 -0700

    Fix duplicate generateSubmoduleContainers/addIdData import in userId_spec.js
    Follow-up to the #14892 conflict resolution: our separately-kept import
    line duplicated a symbol upstream had already merged into the file's
    main userId/index.js import block.

--- test/spec/modules/userId_spec.js
+++ test/spec/modules/userId_spec.js
@@ -37,7 +37,6 @@ import { MODULE_TYPE_UID } from '../../../src/activities/modules.js';
 import { ACTIVITY_ENRICH_EIDS } from '../../../src/activities/activities.js';
 import { ACTIVITY_PARAM_COMPONENT_NAME, ACTIVITY_PARAM_COMPONENT_TYPE } from '../../../src/activities/params.js';
 import { extractEids } from '../../../modules/openadsServerBidAdapter/bidderConfig.js';
-import { generateSubmoduleContainers, addIdData } from '../../../modules/userId/index.js';
 import { registerActivityControl } from '../../../src/activities/rules.js';
 import {
   discloseStorageUse,
