prebid/Prebid.js

Agt 632 migrating modules to type script (#89) (#15436)
* convert all iiq js files to ts

* update version, ts fixes

* fix test and lint issues

* fix lint issues

* no ab group in browserblacklist (#87)

* no ab group in browserblacklist

* fix lint issues

* remove isblacklisted logic from request

* small fix

* add tests

---------



* small fixes

* update version

---------

Co-authored-by: Eyvaz Ahmadzada <eyvazahmadzada@gmail.com>
Co-authored-by: Eyvaz <eyvaz@intentiq.com>

libraries/intentIqUtils/gamPredictionReport.ts
@@ -2,13 +2,17 @@ import { getEvents } from '../../src/events.js';
 import { logError } from '../../src/utils.js';
 import { getSlotTargetingMap } from '../../src/utils/gptTargeting.js';
 
-export function gamPredictionReport (gamObjectReference, sendData) {
+export function gamPredictionReport(
+  gamObjectReference: any,
+  sendData: (data: Record<string, any>) => void
+): void {
   try {
     if (!gamObjectReference || !sendData) {
       logError('Failed to get gamPredictionReport, required data is missed');
       return;
     }
-    const getSlotTargeting = (slot) => {
+
+    const getSlotTargeting = (slot: any): Record<string, string[]> => {
       try {
         return getSlotTargetingMap(slot);
       } catch (e) {
@@ -17,11 +21,11 @@ export function gamPredictionReport (gamObjectReference, sendData) {
       }
     };
 
-    const extractWinData = (gamEvent) => {
+    const extractWinData = (gamEvent: any): Record<string, any> | undefined => {
       const slot = gamEvent.slot;
       const targeting = getSlotTargeting(slot);
 
-      const dataToSend = {
+      const dataToSend: Record<string, any> = {
         placementId: slot.getSlotElementId && slot.getSlotElementId(),
         adUnitPath: slot.getAdUnitPath && slot.getAdUnitPath(),
         bidderCode: targeting.hb_bidder ? targeting.hb_bidder[0] : null,
@@ -30,70 +34,88 @@ export function gamPredictionReport (gamObjectReference, sendData) {
 
       if (dataToSend.placementId) {
         // TODO check auto subscription to prebid events
-        const bidWonEvents = getEvents().filter((ev) => ev.eventType === 'bidWon');
+        const bidWonEvents = getEvents().filter((ev: any) => ev.eventType === 'bidWon');
         if (bidWonEvents.length) {
           for (let i = bidWonEvents.length - 1; i >= 0; i--) {
             const element = bidWonEvents[i];
             if (
               dataToSend.placementId === element.id &&
-                            targeting.hb_adid &&
-                            targeting.hb_adid[0] === element.args.adId
+              targeting.hb_adid &&
+              targeting.hb_adid[0] === (element.args as any).adId
             ) {
-              return; // don't send report if there was bidWon event earlier
+              return;
             }
           }
         }
-        const endEvents = getEvents().filter((ev) => ev.eventType === 'auctionEnd');
+
+        const endEvents = getEvents().filter((ev: any) => ev.eventType === 'auctionEnd');
+
         if (endEvents.length) {
           for (let i = endEvents.length - 1; i >= 0; i--) {
-            // starting from the last event
             const element = endEvents[i];
-            if (element.args?.adUnitCodes?.includes(dataToSend.placementId)) {
-              const defineRelevantData = (bid) => {
-                dataToSend.cpm = bid.cpm + 0.01; // add one cent to the cpm
+
+            if ((element.args as any)?.adUnitCodes?.includes(dataToSend.placementId)) {
+              const defineRelevantData = (bid: any): void => {
+                dataToSend.cpm = bid.cpm + 0.01;
                 dataToSend.currency = bid.currency;
                 dataToSend.originalCpm = bid.originalCpm;
                 dataToSend.originalCurrency = bid.originalCurrency;
                 dataToSend.status = bid.status;
-                dataToSend.prebidAuctionId = element.args?.auctionId;
-                if (!dataToSend.bidderCode) dataToSend.bidderCode = 'GAM';
+                dataToSend.prebidAuctionId = (element.args as any)?.auctionId;
+
+                if (!dataToSend.bidderCode) {
+                  dataToSend.bidderCode = 'GAM';
+                }
               };
+
               if (dataToSend.bidderCode) {
-                const relevantBid = element.args?.bidsReceived.find(
-                  (item) =>
+                const relevantBid = (element.args as any)?.bidsReceived.find(
+                  (item: any) =>
                     item.bidder === dataToSend.bidderCode &&
-                                        item.adUnitCode === dataToSend.placementId
+                    item.adUnitCode === dataToSend.placementId
                 );
+
                 if (relevantBid) {
                   defineRelevantData(relevantBid);
                   break;
                 }
               } else {
                 let highestBid = 0;
-                element.args?.bidsReceived.forEach((bid) => {
-                  if (bid.adUnitCode === dataToSend.placementId && bid.cpm > highestBid) {
+
+                (element.args as any)?.bidsReceived.forEach((bid: any) => {
+                  if (
+                    bid.adUnitCode === dataToSend.placementId &&
+                    bid.cpm > highestBid
+                  ) {
                     highestBid = bid.cpm;
                     defineRelevantData(bid);
                   }
                 });
+
                 break;
               }
             }
           }
         }
       }
+
       return dataToSend;
     };
+
     gamObjectReference.cmd.push(() => {
-      gamObjectReference.pubads().addEventListener('slotRenderEnded', (event) => {
-        if (event.isEmpty) return;
-        const data = extractWinData(event);
-        if (data) {
-          sendData(data);
+      gamObjectReference.pubads().addEventListener(
+        'slotRenderEnded',
+        (event: any) => {
+          if (event.isEmpty) return;
+
+          const data = extractWinData(event);
+          if (data) {
+            sendData(data);
+          }
         }
-      });
+      );
     });
   } catch (error) {
     logError('Failed to subscribe to GAM: ' + error);
   }
-};
+}

OpenAds-Auction/OpenAdsJs

libraries/intentIqUtils/gamPredictionReport.ts
@@ -2,13 +2,17 @@ import { getEvents } from '../../src/events.js';
 import { logError } from '../../src/utils.js';
 import { getSlotTargetingMap } from '../../src/utils/gptTargeting.js';
 
-export function gamPredictionReport (gamObjectReference, sendData) {
+export function gamPredictionReport(
+  gamObjectReference: any,
+  sendData: (data: Record<string, any>) => void
+): void {
   try {
     if (!gamObjectReference || !sendData) {
       logError('Failed to get gamPredictionReport, required data is missed');
       return;
     }
-    const getSlotTargeting = (slot) => {
+
+    const getSlotTargeting = (slot: any): Record<string, string[]> => {
       try {
         return getSlotTargetingMap(slot);
       } catch (e) {
@@ -17,11 +21,11 @@ export function gamPredictionReport (gamObjectReference, sendData) {
       }
     };
 
-    const extractWinData = (gamEvent) => {
+    const extractWinData = (gamEvent: any): Record<string, any> | undefined => {
       const slot = gamEvent.slot;
       const targeting = getSlotTargeting(slot);
 
-      const dataToSend = {
+      const dataToSend: Record<string, any> = {
         placementId: slot.getSlotElementId && slot.getSlotElementId(),
         adUnitPath: slot.getAdUnitPath && slot.getAdUnitPath(),
         bidderCode: targeting.oa_bidder ? targeting.oa_bidder[0] : null,
@@ -30,70 +34,88 @@ export function gamPredictionReport (gamObjectReference, sendData) {
 
       if (dataToSend.placementId) {
         // TODO check auto subscription to prebid events
-        const bidWonEvents = getEvents().filter((ev) => ev.eventType === 'bidWon');
+        const bidWonEvents = getEvents().filter((ev: any) => ev.eventType === 'bidWon');
         if (bidWonEvents.length) {
           for (let i = bidWonEvents.length - 1; i >= 0; i--) {
             const element = bidWonEvents[i];
             if (
               dataToSend.placementId === element.id &&
-                            targeting.oa_adid &&
-                            targeting.oa_adid[0] === element.args.adId
+              targeting.oa_adid &&
+              targeting.oa_adid[0] === (element.args as any).adId
             ) {
-              return; // don't send report if there was bidWon event earlier
+              return;
             }
           }
         }
-        const endEvents = getEvents().filter((ev) => ev.eventType === 'auctionEnd');
+
+        const endEvents = getEvents().filter((ev: any) => ev.eventType === 'auctionEnd');
+
         if (endEvents.length) {
           for (let i = endEvents.length - 1; i >= 0; i--) {
-            // starting from the last event
             const element = endEvents[i];
-            if (element.args?.adUnitCodes?.includes(dataToSend.placementId)) {
-              const defineRelevantData = (bid) => {
-                dataToSend.cpm = bid.cpm + 0.01; // add one cent to the cpm
+
+            if ((element.args as any)?.adUnitCodes?.includes(dataToSend.placementId)) {
+              const defineRelevantData = (bid: any): void => {
+                dataToSend.cpm = bid.cpm + 0.01;
                 dataToSend.currency = bid.currency;
                 dataToSend.originalCpm = bid.originalCpm;
                 dataToSend.originalCurrency = bid.originalCurrency;
                 dataToSend.status = bid.status;
-                dataToSend.prebidAuctionId = element.args?.auctionId;
-                if (!dataToSend.bidderCode) dataToSend.bidderCode = 'GAM';
+                dataToSend.prebidAuctionId = (element.args as any)?.auctionId;
+
+                if (!dataToSend.bidderCode) {
+                  dataToSend.bidderCode = 'GAM';
+                }
               };
+
               if (dataToSend.bidderCode) {
-                const relevantBid = element.args?.bidsReceived.find(
-                  (item) =>
+                const relevantBid = (element.args as any)?.bidsReceived.find(
+                  (item: any) =>
                     item.bidder === dataToSend.bidderCode &&
-                                        item.adUnitCode === dataToSend.placementId
+                    item.adUnitCode === dataToSend.placementId
                 );
+
                 if (relevantBid) {
                   defineRelevantData(relevantBid);
                   break;
                 }
               } else {
                 let highestBid = 0;
-                element.args?.bidsReceived.forEach((bid) => {
-                  if (bid.adUnitCode === dataToSend.placementId && bid.cpm > highestBid) {
+
+                (element.args as any)?.bidsReceived.forEach((bid: any) => {
+                  if (
+                    bid.adUnitCode === dataToSend.placementId &&
+                    bid.cpm > highestBid
+                  ) {
                     highestBid = bid.cpm;
                     defineRelevantData(bid);
                   }
                 });
+
                 break;
               }
             }
           }
         }
       }
+
       return dataToSend;
     };
+
     gamObjectReference.cmd.push(() => {
-      gamObjectReference.pubads().addEventListener('slotRenderEnded', (event) => {
-        if (event.isEmpty) return;
-        const data = extractWinData(event);
-        if (data) {
-          sendData(data);
+      gamObjectReference.pubads().addEventListener(
+        'slotRenderEnded',
+        (event: any) => {
+          if (event.isEmpty) return;
+
+          const data = extractWinData(event);
+          if (data) {
+            sendData(data);
+          }
         }
-      });
+      );
     });
   } catch (error) {
     logError('Failed to subscribe to GAM: ' + error);
   }
-};
+}